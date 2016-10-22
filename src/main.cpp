5a340d189c0c69160eb891150dddb5df318ed9b2
Petr Kolář <Petr Kol>
2016-10-22 20:42:54 +0200

    do not blink at start

--- src/main.cpp
+++ src/main.cpp
@@ -56,6 +56,8 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
+  blinker.stop();
+
   client.sentCallback(handleDataSent);
 
   CustomWiFiManager::start(&blinker);
